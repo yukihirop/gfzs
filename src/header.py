45e4fc2dc74f6893780e6702630d676261352ee4
yukihirop <te108186@gmail.com>
2021-01-16 14:57:09 +0900

    Change google color about Header

--- src/header.py
+++ src/header.py
@@ -1,5 +1,7 @@
 import curses
 
+GOOGLE = 'Google'
+
 class Header:
   def __init__(self, stdscr, colors):
     self.title = 'Google Fuzzy Search (v0.0.1)'
@@ -35,6 +37,25 @@ class Header:
   # https://stackoverflow.com/a/53016371/9434894
   def _make_header(self):
     self.window.addstr(0, 0, self.title, self.colors.header | curses.A_BOLD)
+    
+    # Override Google
+    google = list(GOOGLE)
+    first_o = True
+    for i in range(len(google)):
+      c = google[i]
+      if c in ('G', 'g'):
+        self.window.addstr(0, i, c, self.colors.googler_g | curses.A_BOLD)
+      elif c == 'o':
+        if first_o:
+          first_o = False
+          self.window.addstr(0, i, c, self.colors.googler_o | curses.A_BOLD)
+        else:
+          self.window.addstr(0, i, c, self.colors.googler_o2 | curses.A_BOLD)
+      elif c == 'l':
+        self.window.addstr(0, i, c, self.colors.googler_l | curses.A_BOLD)
+      elif c == 'e':
+        self.window.addstr(0, i, c, self.colors.googler_e | curses.A_BOLD)
+
     self.window.addstr(0, self.parent_width - len(self.copyright),
                        self.copyright, self.colors.header | curses.A_BOLD)
     self.window.hline(1, 0, curses.ACS_HLINE | self.colors.hline, self.parent_width)
