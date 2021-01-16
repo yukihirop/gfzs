996bfedc1378c369cc096c6a8331fdd1113bf687
yukihirop <te108186@gmail.com>
2021-01-16 14:34:13 +0900

    Implement debug

---
+++ src/debug.py
@@ -0,0 +1,13 @@
+import pprint
+
+pp = pprint.PrettyPrinter(indent=2)
+
+def log(name, s= ""):
+  with open("./tmp/eg_googler.log", "a") as f:
+    f.write(str(name) + ":" + str(s) + "\n")
+
+def dump(obj):
+  with open("./tmp/eg_googler.log", "a") as f:
+    f.write(pp.pformat(obj) + "\n")
+    
+  return obj
