80ca2752ba475b400d63c671194e22a26e5e243b
yukihirop <te108186@gmail.com>
2021-01-19 23:40:29 +0900

    Modify config

--- gfzs/config.py
+++ gfzs/config.py
@@ -1,19 +1,33 @@
 # ref: https://qiita.com/ttsubo/items/c4af71ceba15b5b213f8
 
+import argparse
+
+DEFAULT_SCORE = 30
+
+default_args = argparse.Namespace(
+    score=DEFAULT_SCORE
+)
+
 class Singleton(object):
     @classmethod
-    def get_instance(cls, args = None):
+    def get_instance(cls, args = default_args):
         if not hasattr(cls, "_instance"):
             cls._instance = cls(args)
         else:
             if args != None:
               cls._instance.args = args
+
         return cls._instance
 
 class Config(Singleton):
+    """A class that reads and manages the options passed in the runtime"""
+  
+    """fuzzywuzzy's score. please see https://github.com/seatgeek/fuzzywuzzy"""
+    default_score = DEFAULT_SCORE
+
     def __init__(self, args):
         self.args = args
 
     @property
     def score(self):
-      return self.args.score
+      return self.args.score or Config.default_score
