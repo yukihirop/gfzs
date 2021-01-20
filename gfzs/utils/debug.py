3d3ef27c540e7806197b45edc2ff9bd9d4ad3405
yukihirop <te108186@gmail.com>
2021-01-20 20:57:33 +0900

    Rename log name in development

--- gfzs/utils/debug.py
+++ gfzs/utils/debug.py
@@ -3,11 +3,11 @@ import pprint
 pp = pprint.PrettyPrinter(indent=2)
 
 def log(name, s= ""):
-  with open("./tmp/eg_googler.log", "a") as f:
+  with open("./tmp/gfzs.log", "a") as f:
     f.write(str(name) + ":" + str(s) + "\n")
 
 def dump(obj):
-  with open("./tmp/eg_googler.log", "a") as f:
+  with open("./tmp/gfzs.log", "a") as f:
     f.write(pp.pformat(obj) + "\n")
     
   return obj
